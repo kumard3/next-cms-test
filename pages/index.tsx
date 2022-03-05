031f5301866744da5d392d14d25629b43ca6fc40
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-05 19:12:19 +0530

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { GetServerSideProps, NextPage } from "next";
 
+export default function Home({ res }: any) {
 
-export default function Home() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        "https://astroyantra.com/wp-json/wp/v2/posts/"
-      );
-
-      setData(result.data);
-    };
-
-    fetchData();
-  }, []);
+  console.log(res);
   return (
     <div className="bg-black min-h-screen text-white/80">
       <h1 className="text-center text-6xl py-10 ">
         Remix Basic Data fetching{" "}
       </h1>
       <div className="grid justify-center flex-col items-center  ">
-        {data?.map((n: any,index) => {
+        {res?.map((n: any, index: React.Key | null | undefined) => {
           return (
             <div key={index}>
               <main dangerouslySetInnerHTML={{ __html: n.content.rendered }} />
             </div>
           );
         })}
-     </div>
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const result = await fetch("https://astroyantra.com/wp-json/wp/v2/posts/");
+  const res = await result.json();
+
+  return {
+    props: res,
+  };
+};
